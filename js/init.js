1d9d418cd3def4987c9d9f20adcfa627e5897a63
Dominic Dominguez <i@dominicjames.co>
2022-01-27 12:08:21 -0600

    Add Galleries Folder

--- js/init.js
+++ js/init.js
@@ -1,5 +1,30 @@
+var walk = function(dir, done) {
+	var results = [];
+	fs.readdir(dir, function(err, list) {
+	  if (err) return done(err);
+	  var i = 0;
+	  (function next() {
+		var file = list[i++];
+		if (!file) return done(null, results);
+		file = path.resolve(dir, file);
+		fs.stat(file, function(err, stat) {
+		  if (stat && stat.isDirectory()) {
+			walk(file, function(err, res) {
+			  results = results.concat(res);
+			  next();
+			});
+		  } else {
+			results.push(file);
+			next();
+		  }
+		});
+	  })();
+	});
+  };
 (function($) {
     "use strict";
+	walk('/images/galleries');
+
 
     // home slider
     $('.home-slider').owlCarousel({
